e33930b4f49ebe16e0b6b7a66e8f9b2efb5d534c
Ian Lomas <you@example.com>
2025-09-21 14:53:17 -0400

    strucvture

--- js/scenes/titleScene.js
+++ js/scenes/titleScene.js
@@ -7,11 +7,19 @@ let titleScene = new Phaser.Class({
   },
 
   preload: function () {
+    this.load.image('ball', 'images/ball.png');
   },
 
   create: function () {
     scene = this;
+    
+this.matter.world.setBounds(
+  0, 0, GAME_WIDTH, GAME_HEIGHT,
+  32,  // thickness of the walls
+  true, true, true, true // left, right, top, bottom enabled
+);
     new Leaf(200, 200);
+    new Ball(200, 150);
   },
 
   update: function (time) {
