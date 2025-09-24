f9a1031c97a4ce9ffe171e057d1302c91083fe38
Ian Lomas <you@example.com>
2025-09-24 14:23:09 -0400

    argih

--- js/classes/paddle.js
+++ js/classes/paddle.js
@@ -1,17 +1,20 @@
 class Paddle extends Entity {
   constructor(x, y) {
     super(x, y);
-    this.leftSprite = scene.add.image(-50, 0, "paddleLeft");
+    this.leftSprite = scene.add.image(-50, -20, "paddleLeft");
+    this.leftSprite.setScale(1, .4)
     this.add(this.leftSprite);
-    this.middleSprite = scene.add.image(0, 0, "paddleMiddle");
+    this.middleSprite = scene.add.image(0, -20, "paddleMiddle");
+    this.middleSprite.setScale(1, .4)
     this.add(this.middleSprite);
-    this.rightSprite = scene.add.image(50, 0, "paddleRight");
+    this.rightSprite = scene.add.image(50, -20, "paddleRight");
+    this.rightSprite.setScale(1, .4)
     this.add(this.rightSprite);
 
     this.width = 150;
     this.height = 50;
 
-    this.body = scene.matter.add.rectangle(x, y, this.width, this.height, {
+    this.body = scene.matter.add.rectangle(x, y + 20, this.width, this.height, {
       chamfer: { radius: 35 },
       restitution: 0,
       friction: 0,
@@ -24,7 +27,7 @@ class Paddle extends Entity {
     scene.input.on("pointermove", (pointer) => {
       const halfWidth = this.width / 2;
       let targetX = Phaser.Math.Clamp(pointer.x, halfWidth, GAME_WIDTH - halfWidth);
-      scene.matter.body.setPosition(this.body, { x: targetX, y: y });
+      scene.matter.body.setPosition(this.body, { x: targetX, y: y + 20 });
       this.setPosition(this.body.position.x, this.body.position.y);
     });
   }
